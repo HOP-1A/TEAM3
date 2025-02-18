d0ef9b3a44b9b4566fbea6ccf90b8504d93b638d
24HP1905 24HP1905 <24HP1905@admins-iMac.local>
2025-02-18 16:07:03 +0800

    header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="bg-red-400 h-fit">
+        <div className="text-white">Хүссэнээ зар, Хайснаа ол</div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Header;
